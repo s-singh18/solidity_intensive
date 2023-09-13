bb673a86ee5ad87d26f6afa00f946049e5ac0a50
s-singh18 <65569092+s-singh18@users.noreply.github.com>
2023-09-12 20:05:02 -0700

    Added errors module

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Errors1 {
-
     event Log(string message);
 
     function example1(uint _value) public {
@@ -16,24 +15,21 @@ contract Errors1 {
         if (_value <= 10) {
             revert("must be greater than 10");
         }
+
         emit Log("success");
     }
 
-    // Usually checks internal values...
     function example3(uint _value) public {
         assert(_value == 10);
         emit Log("success");
     }
 
-    // Custom error
     error InvalidValue(uint value);
 
     function example4(uint _value) public {
         if (_value <= 10) {
-            // You can add more arguments to this...
-            revert InvalidValue({value: _value });
+            revert InvalidValue({value: _value});
         }
         emit Log("success");
     }
 }
-
